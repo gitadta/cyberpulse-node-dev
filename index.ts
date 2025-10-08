ab784da3ffc8621c7ad5ed61b2fcf8d1b3196d3a
gitadta <209597669+gitadta@users.noreply.github.com>
2025-10-09 10:40:38 +1100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { CyberPulseCompliance } from './nodes/CyberPulseCompliance/CyberPulseCompliance.node';
+import { CyberPulseHttpHeaderAuth } from './credentials/CyberPulseHttpHeaderAuth.credentials';
+
+export const nodes = [CyberPulseCompliance];
+export const credentials = [CyberPulseHttpHeaderAuth];
